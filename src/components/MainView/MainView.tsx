50eb8338d96fe2efa1092583fbd30e165c552743
MaitaJv <javiermaita@hotmail.com.ar>
2025-07-01 01:11:21 -0300

    efecto de tipado en el titulo

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import './MainView.css'
 
 const MainView = () => {
+  const full_title: string = "Javier Maita"
+  let aux:string = ""
+  const [title, setTitle] = useState<string>("")
+
+  useEffect(()=>{
+    let index: number = 0
+    const interval = setInterval(() => {
+      console.log("index: ", index);
+      aux = aux + full_title[index]
+      setTitle(aux)
+      index++
+      if (index === (full_title.length)) clearInterval(interval)
+    }, 150);
+
+    return () => clearInterval(interval);
+  }, [])
+
   return (
     <div className='main_view'>
       <div className='titles_container_main'>
-        <h1>Javier Maita</h1>
+        <h1>{title}</h1>
         <h2>Técnico electronico | Estudiante de Ingenieria en Sistemas | Front End Developer</h2>
       </div>
       <div className='scroll_down_main' >
